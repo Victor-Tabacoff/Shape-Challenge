92710b3bf5b1757a87feabbd667a527b24145a03
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-08-22 22:48:11 -0500

    Initial Commit

---
+++ Shape Challenge/Shape_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shape_ChallengeApp.swift
+//  Shape Challenge
+//
+//  Created by Victor Tabacoff on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Shape_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
